a3719b0e05217e99a0de3ab0bfe8fe03a75d4de4
jusztin <jusztinp@gmail.com>
2020-08-11 21:26:50 +0200

    Add models and populate application properties

---
+++ src/main/java/com/justiny/rpete/RpeteApplication.java
@@ -0,0 +1,13 @@
+package com.justiny.rpete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justiny/rpete/RpeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justiny.rpete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
